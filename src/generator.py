e3228adf2af8e18da9dc9545caf5a6db13406f88
Caires & Victor Kendy Harada <victorkendy@gmail.com>
2011-04-11 10:50:37 -0300

    added generator function example

---
+++ src/generator.py
@@ -0,0 +1,11 @@
+# Generator function
+# http://diveintopython.org/dynamic_functions/stage6.html
+
+def infinite_list(start=1):
+    current = start
+    while (True):
+        yield current
+        current = current + 1
+
+for i in infinite_list(start=2):
+    print i
